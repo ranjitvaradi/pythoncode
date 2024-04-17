c27902ec771b9d5c8f576228054dcb56f478b143
ranjit.vnv <ranjit.vnv@gmail.com>
2024-04-17 06:49:47 +0530

    new

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,35 @@
-n=6
-for i in range (1,n+1):
-   for j in range (1,n+2-i):
 
-      print(n+1-j,end=" ")
-   print()
+def is_palindrome(word):
+   l=[]
+   for x in word:
+      l.append(x)
+   k=list(l[::-1])
+   if k==l:
+      return True
+   else:
+      return False
+y=is_palindrome(input("enter a word: "))
+print(y)
    
+
+
+
+
+
+
+
+   
+   
+   
+   
+   
+
+   
+   
+         
+
+         
+         
+   
+      
+
